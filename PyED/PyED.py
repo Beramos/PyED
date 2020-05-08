4bc0e951e4420ee1f2c0c5994996063236f4bc14
beramos <bram.de.jaegher@gmail.com>
2020-05-08 11:13:38 +0200

    init first workable version of model

---
+++ PyED/PyED.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 08 10:50:00 2020
+@author: Bram De Jaegher
+""" 
+
+class Electrolyte:
+    def __init__(self, lambdaEq=10.53e-3):
+        self.lambdaEq = lambdaEq
+
+
+class Reservoir:
+    def __init__(self, volume, concentration, electrolyte=Electrolyte()):
+        self.volume = volume
+        self.electrolyte = electrolyte
+        self.concentration = concentration
+    
+    def timeLoop(self, flux, time):
+        return self.concentration - time*flux/self.volume
+
+    def conductivity(self, concentration, electrolyte=self.electrolyte)
+        return = concentration*electrolyte.lambdaEq
+
+
+class Channel:
+    def __init__(self, length, width, thickness, electrolyte=Electrolyte()):
+        self.length = length
+        self.width = width
+        self.thickness = thickness
+        self.surfaceMem = length*width
+        self.volume = length*width*thickness
+        self.electrolyte
+
+    def conductivity(self, concentration, electrolyte=self.electrolyte)
+        return = concentration*electrolyte.lambdaEq
+        
+    def resistance(self, concentration, electrolyte=self.electrolyte):
+        kappa = self.conductivity(concentration, electrolyte=electrolyte)
+        resistanceSolution = self.thickness/kappa/self.surfaceMem
+        return resistanceSolution
