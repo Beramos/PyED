cc0f23c5e4bdc3ec98e05ae08e9599bcf25d92c2
beramos <bram.de.jaegher@gmail.com>
2020-05-08 16:28:38 +0200

    added method for temperature-dependent conductivity

--- PyED/PyED.py
+++ PyED/PyED.py
@@ -4,6 +4,8 @@ Created on Fri May 08 10:50:00 2020
 @author: Bram De Jaegher
 """ 
 
+import numpy as np
+
 class Electrolyte:
     def __init__(self, lambdaEq=10.53e-3):
         self.lambdaEq = lambdaEq
@@ -19,6 +21,15 @@ class Reservoir:
         return self.concentration - time*flux/self.volume
 
     def conductivity(self, concentration):
+        return concentration*self.electrolyte.lambdaEq
+
+    def conductivityTemp(self, concentration, temperature):
+        """Source: 
+        https://www.mt.com/dam/mt_ext_files/Editorial/Generic/4/Paper-THOR-Cation-Cond-Temp-Bev-Gray-11-97_Editorial-Generic_1161617581924_files/cation_cond_tempcompensation.pdf
+        """
+    Cw = 0.0545*(0.55*np.exp(0.0363*temperature)-0.356)
+    CT = (C25 - 0.0545)*(1+0.02*(temperature-25))+ Cw
+    return CT*concen
         return concentration*self.electrolyte.lambdaEq
 
 
